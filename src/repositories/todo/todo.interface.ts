622d1c624c52b21a6a5077ec87a52c433f303e83
Vadim <mrdrakan@Mail.ru>
2022-04-06 16:04:20 +0300

    feat(todo-repository): Updated repository, added creation request.

--- src/repositories/todo/todo.interface.ts
+++ src/repositories/todo/todo.interface.ts
@@ -7,4 +7,15 @@ export interface ITodoFetchResponse {
 
 export interface ITodoRepository {
     fetch(): Promise<Todo[] | false>;
+    setCompleted(id: number, state: boolean): Promise<boolean>;
+    create(content: string, scheduled?: number): Promise<Todo | false>;
+}
+
+export interface ITodoSetCompletedResponse {
+    status: boolean;
+}
+
+export interface ITodoCreateResponse {
+    status: boolean;
+    data?: Todo;
 }
--- src/repositories/todo/todo.ts
+++ src/repositories/todo/todo.ts
@@ -1,6 +1,6 @@
 import { Todo } from "../../types/todo/todo";
 import { api } from "../api";
-import type { ITodoFetchResponse, ITodoRepository } from "./todo.interface";
+import type { ITodoCreateResponse, ITodoFetchResponse, ITodoRepository, ITodoSetCompletedResponse } from "./todo.interface";
 
 export class TodoRepository implements ITodoRepository {
     async fetch(): Promise<Todo[] | false> {
@@ -12,10 +12,21 @@ export class TodoRepository implements ITodoRepository {
         return false;
     }
     async setCompleted(id: number, state: boolean): Promise<boolean> {
-        const result: false | ITodoFetchResponse = await api.put(`/todo/${id}/completed?state=${state === true ? 'true' : 'false'}`)
+        const result: false | ITodoSetCompletedResponse = await api.put(`/todo/${id}/completed?state=${state === true ? 'true' : 'false'}`)
         .then((res) => res.data);
         if(result !== false && result.status === true){
             return true
+        }
+        return false;
+    }
+    async create(content: string, scheduled?: number): Promise<Todo | false> {
+        const result: false | ITodoCreateResponse = await api.post('/todo/create', {
+            content,
+            scheduled
+        })
+        .then((res) => res.data);
+        if(result !== false && result.status === true && result.data !== undefined){
+            return new Todo(result.data);
         }
         return false;
     }
