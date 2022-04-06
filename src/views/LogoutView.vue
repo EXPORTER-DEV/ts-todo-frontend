8504fdcbccf2916097821561fdc0b9a49596063a
Vadim <mrdrakan@Mail.ru>
2022-04-06 16:03:49 +0300

    feat(LogoutView): Added notify.

--- src/views/LogoutView.vue
+++ src/views/LogoutView.vue
@@ -4,14 +4,22 @@
     import { useStore } from 'vuex';
     import { RepositoriesKey, type IRepositories } from '../repositories';
     import { StoreKey } from '../store';
+    import { ToastControl } from '../components/toast/toast-control';
+    import { ToastEnum } from '../types/toast/toast.interface';
     
     const repositories = inject<IRepositories>(RepositoriesKey);
     const router = useRouter();
     const store = useStore(StoreKey);
 
+    const toastControl = new ToastControl();
+
     onMounted(async () => {
         store.dispatch('setUser', false);
         await repositories!.authRepository.logout();
+        toastControl.create({
+            body: `Successfully logged out.`,
+            type: ToastEnum.SUCCESS,
+        });
         router.push('/');
     });
 </script>
