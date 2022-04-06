97c5525cbfde038a2cba376db66f41a2ddffbf59
Vadim <mrdrakan@Mail.ru>
2022-04-06 16:05:06 +0300

    feat(TodoView): Added notify, added support of CreateTodo component.

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -11,6 +11,7 @@
 
 	import IconCircleOutline from '~icons/mdi/circle-outline';
 	import IconCheckboxCircle from '~icons/mdi/checkbox-multiple-marked-circle';
+	import CreateTodo from '../components/todo/CreateTodo.vue';
 
 	moment.locale('en');
 	
@@ -24,14 +25,15 @@
 
 	const load = async () => {
 		store.dispatch('setLoadingActive', true);
-		const items = await repositories?.todoRepository.fetch()
-			.catch((e) => {
+		const items = await repositories!.todoRepository.fetch()
+			.catch((e): false => {
 				toastControl.create({
 					body: 'Failed load todo list',
 					type: ToastEnum.FAILED,
 				})
+				return false;
 			});
-		if(items !== false && items instanceof Array){
+		if(items !== false){
 			loaded.value = true;
 			todoList.value = items;
 		}
@@ -39,7 +41,7 @@
 	}
 
 	const setCompleted = async (id: number, state: boolean = true) => {
-		const result = await repositories?.todoRepository.setCompleted(id, state)
+		const result = await repositories!.todoRepository.setCompleted(id, state)
 			.catch((e) => {
 				toastControl.create({
 					body: 'Failed to update todo',
@@ -50,12 +52,20 @@
 			const item = todoList.value.find((item) => item.id === id);
 			if(item !== undefined){
 				item.completed = state;
+				toastControl.create({
+					body: `Task marked as ${state === true ? 'completed' : 'uncompleted'}.`,
+					type: ToastEnum.SUCCESS,
+				})
 			}
 		}
 	}
 
 	const formatTimestamp = (timestamp: number): string => {
 		return moment(timestamp).format('Do MMMM YYYY HH:mm');
+	}
+
+	const createdTodo = (todo: Todo) => {
+		todoList.value.push(todo);
 	}
 
 	const completed = computed<Todo[]>(() => todoList.value.filter((item) => item.completed === true));
@@ -68,80 +78,83 @@
 
 <template>
 	<div class="container content" v-show="loaded">
-		<h3>Active ({{active.length}})</h3>
-		<br>
-		<table class="table">
-			<thead>
-				<tr>
-					<th scope="col">Completed</th>
-					<th scope="col">Content</th>
-					<th scope="col">Created at</th>
-				</tr>
-			</thead>
-			<tbody v-if="active.length > 0">
-				<tr v-for="item in active">
-					<th class="clickable" @click="setCompleted(item.id, true)"><IconCircleOutline></IconCircleOutline></th>
-					<td>{{item.content}}</td>
-					<td class="text-muted">{{formatTimestamp(item.created_at)}}</td>
-				</tr>
-			</tbody>
-			<tbody v-else>
-				<tr>
-					<th colspan="3" class="text-center fs-6 text fw-light">No active todos.</th>
-				</tr>
-			</tbody>
-		</table>
-		<h3>Scheduled ({{scheduled.length}})</h3>
-		<br>
-		<table class="table">
-			<thead>
-				<tr>
-					<th scope="col">Completed</th>
-					<th scope="col">Content</th>
-					<th scope="col">Scheduled at</th>
-				</tr>
-			</thead>
-			<tbody v-if="scheduled.length > 0">
-				<tr v-for="item in scheduled">
-					<th class="clickable" @click="setCompleted(item.id, true)"><IconCircleOutline></IconCircleOutline></th>
-					<td>{{item.content}}</td>
-					<td class="text-muted">{{formatTimestamp(item.scheduled)}}</td>
-				</tr>
-			</tbody>
-			<tbody v-else>
-				<tr>
-					<th colspan="3" class="text-center fs-6 text fw-light">No scheduled todos.</th>
-				</tr>
-			</tbody>
-		</table>
-		<h3>Completed ({{completed.length}})</h3>
-		<br>
-		<table class="table">
-			<thead>
-				<tr>
-					<th scope="col">Completed</th>
-					<th scope="col">Content</th>
-					<th scope="col">Completed at</th>
-				</tr>
-			</thead>
-			<tbody v-if="completed.length > 0">
-				<tr v-for="item in completed">
-					<th class="clickable" @click="setCompleted(item.id, false)"><IconCheckboxCircle></IconCheckboxCircle></th>
-					<td>{{item.content}}</td>
-					<td class="text-muted">{{formatTimestamp(item.created_at)}}</td>
-				</tr>
-			</tbody>
-			<tbody v-else>
-				<tr>
-					<th colspan="3" class="text-center fs-6 text fw-light">No completed todos.</th>
-				</tr>
-			</tbody>
-		</table>
+		<div class="body">
+			<h3>Active ({{active.length}})</h3>
+			<br>
+			<table class="table">
+				<thead>
+					<tr>
+						<th scope="col">Completed</th>
+						<th scope="col">Content</th>
+						<th scope="col">Created at</th>
+					</tr>
+				</thead>
+				<tbody v-if="active.length > 0">
+					<tr v-for="item in active">
+						<th class="clickable" @click="setCompleted(item.id, true)"><IconCircleOutline></IconCircleOutline></th>
+						<td>{{item.content}}</td>
+						<td class="text-muted">{{formatTimestamp(item.created_at)}}</td>
+					</tr>
+				</tbody>
+				<tbody v-else>
+					<tr>
+						<th colspan="3" class="text-center fs-6 text fw-light">No active todos.</th>
+					</tr>
+				</tbody>
+			</table>
+			<h3>Scheduled ({{scheduled.length}})</h3>
+			<br>
+			<table class="table">
+				<thead>
+					<tr>
+						<th scope="col">Completed</th>
+						<th scope="col">Content</th>
+						<th scope="col">Scheduled at</th>
+					</tr>
+				</thead>
+				<tbody v-if="scheduled.length > 0">
+					<tr v-for="item in scheduled">
+						<th class="clickable" @click="setCompleted(item.id, true)"><IconCircleOutline></IconCircleOutline></th>
+						<td>{{item.content}}</td>
+						<td class="text-muted">{{formatTimestamp(item.scheduled)}}</td>
+					</tr>
+				</tbody>
+				<tbody v-else>
+					<tr>
+						<th colspan="3" class="text-center fs-6 text fw-light">No scheduled todos.</th>
+					</tr>
+				</tbody>
+			</table>
+			<h3>Completed ({{completed.length}})</h3>
+			<br>
+			<table class="table">
+				<thead>
+					<tr>
+						<th scope="col">Completed</th>
+						<th scope="col">Content</th>
+						<th scope="col">Completed at</th>
+					</tr>
+				</thead>
+				<tbody v-if="completed.length > 0">
+					<tr v-for="item in completed">
+						<th class="clickable" @click="setCompleted(item.id, false)"><IconCheckboxCircle></IconCheckboxCircle></th>
+						<td>{{item.content}}</td>
+						<td class="text-muted">{{formatTimestamp(item.created_at)}}</td>
+					</tr>
+				</tbody>
+				<tbody v-else>
+					<tr>
+						<th colspan="3" class="text-center fs-6 text fw-light">No completed todos.</th>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<CreateTodo @created="createdTodo"></CreateTodo>
 	</div>
 </template>
 
 <style lang="scss">
-	th.clickable {
+	.clickable {
 		cursor: pointer;
 	}
 </style>
